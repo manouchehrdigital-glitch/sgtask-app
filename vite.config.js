1707e7d775cee32c267873ea3038f8d15e0b4922
Manouchehr <manouchehr@Manouchehrs-MacBook-Pro.local>
2025-12-04 21:39:56 +0330

    Fix configuration files

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
+  server: {
+    port: 3000,
+  }
 })
